6b42c31e391d61fdee406e52b0942f6b069f0eea
turtletennis <stephenjenkins42@gmail.com>
2025-01-26 15:23:52 +0000

    Change game over to load menu when button pressed instead of quit

--- GameOver/GameOver.cs
+++ GameOver/GameOver.cs
@@ -5,12 +5,14 @@ public partial class GameOver : Control
 {
 	[Export] private Label _gameOverLabel;
 	[Export] private Button _mainMenuButton;
+	[Export] private string _mainMenuScene = "res://MainMenu/MainMenu.tscn";
 
 	public override void _Ready()
 	{
 		_gameOverLabel.Text = Game.Winner == 1 ? "Player One wins!" : "Player Two wins!";
 		_mainMenuButton.Pressed += () => {
-			GetTree().Quit();
+			GetTree().ChangeSceneToFile(_mainMenuScene);
 		};
+		//GetTree().ChangeSceneToFile(_mainMenuScene);
 	}
 }
